0081a0f2ad0cd6e123a8903d3f374671349f3fe8
G-Jeffrey <jeffreyg2240@gmail.com>
2023-03-01 01:33:19 -0500

    Updates

---
+++ precache-manifest.1e8e8af2f484b5360db7e355d4030578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2d5a48bf6876a8780f2",
+    "url": "/Portfolio/static/css/main.838f147e.chunk.css"
+  },
+  {
+    "revision": "c2d5a48bf6876a8780f2",
+    "url": "/Portfolio/static/js/main.c2d5a48b.chunk.js"
+  },
+  {
+    "revision": "4eb855c0b5ed0c27c489",
+    "url": "/Portfolio/static/js/1.4eb855c0.chunk.js"
+  },
+  {
+    "revision": "dfadda34cbe2705bc593",
+    "url": "/Portfolio/static/css/2.b3fce81e.chunk.css"
+  },
+  {
+    "revision": "dfadda34cbe2705bc593",
+    "url": "/Portfolio/static/js/2.dfadda34.chunk.js"
+  },
+  {
+    "revision": "94b8af411ef82f5851af",
+    "url": "/Portfolio/static/js/runtime~main.94b8af41.js"
+  },
+  {
+    "revision": "dbc95e35c3e28ba2d25f6271ba0c0c3c",
+    "url": "/Portfolio/index.html"
+  }
+];
